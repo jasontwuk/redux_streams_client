cfe482cbf4dfcbe7f0cd62aecfb9fbb8a58315ce
Jason <jasontwuk@gmail.com>
2021-05-07 13:58:51 +0100

    show a stream

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Router, Route } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 import Header from "./Header";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
@@ -13,11 +13,13 @@ const App = () => {
       <Router history={history}>
         <div>
           <Header />
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" component={StreamCreate} />
-          <Route path="/streams/edit/:id" component={StreamEdit} />
-          <Route path="/streams/delete/:id" component={StreamDelete} />
-          <Route path="/streams/show" component={StreamShow} />
+          <Switch>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" component={StreamCreate} />
+            <Route path="/streams/edit/:id" component={StreamEdit} />
+            <Route path="/streams/delete/:id" component={StreamDelete} />
+            <Route path="/streams/:id" component={StreamShow} />
+          </Switch>
         </div>
       </Router>
     </div>
